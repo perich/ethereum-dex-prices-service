22e5811f9d34bc434d80734c50996289e8476a8f
Graham Perich <graham.perich@gmail.com>
2018-12-24 13:20:56 -0800

    refactor folder structure

---
+++ exchanges/IDEX.js
@@ -0,0 +1,111 @@
+const rp = require('request-promise')
+const { IDEX_URL } = require('../constants.js')
+
+module.exports = class IDEX {
+  constructor() {
+    this.url = `${IDEX_URL}/returnOrderBook`
+    this.name = 'IDEX'
+  }
+
+  // fetch the raw order book from the exchange
+  getRawOrderBook(symbol) {
+    const body = {
+      market: `ETH_${symbol}`,
+      count: 50,
+    }
+    const config = {
+      uri: this.url,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body,
+      json: true,
+    }
+
+    return rp(config)
+  }
+
+  // create an order book that conforms to the generalized order book interface
+  async createCanonicalOrderBook(symbol) {
+    let lotPrice = 0
+    let lotAmount = 0
+    function walkBook(level) {
+      const levelPrice = parseFloat(level.price)
+      const levelAmount = parseFloat(level.amount)
+
+      lotAmount += levelAmount
+      lotPrice += levelPrice * levelAmount
+
+      const avgUnitPrice = lotPrice / lotAmount
+      return {
+        levelPrice,
+        levelAmount,
+        lotPrice,
+        lotAmount,
+      }
+    }
+
+    return new Promise(async resolve => {
+      try {
+        const book = await this.getRawOrderBook(symbol)
+        const { asks, bids } = book
+
+        const formattedAsks = asks.map(walkBook)
+        lotPrice = 0
+        lotAmount = 0
+        const formattedBids = bids.map(walkBook)
+        resolve({ asks: formattedAsks, bids: formattedBids })
+      } catch (error) {
+        resolve(null)
+      }
+    })
+  }
+
+  // compute the average token price based on DEX liquidity and desired token amount
+  async computePrice(symbol, desiredAmount) {
+    const book = await this.createCanonicalOrderBook(symbol)
+    let result = {}
+    if (!book) {
+      return {
+        [this.name]: new Error(
+          `no price data found on ${this.name} for ${symbol}`,
+        ),
+      }
+    } else {
+      const { asks } = book
+      let avgPrice = 0
+
+      for (let i = 0; i < asks.length; i++) {
+        const ask = asks[i]
+        const prevAsk = asks[i - 1]
+
+        if (desiredAmount > ask.lotAmount) {
+          continue
+        }
+
+        if (i === 0) {
+          avgPrice = ask.levelPrice
+          break
+        }
+        const remainder = desiredAmount - prevAsk.lotAmount
+        const totalPrice = prevAsk.lotPrice + remainder * ask.levelPrice
+        avgPrice = totalPrice / desiredAmount
+        break
+      }
+
+      // book didn't have enough liquidity for this size order
+      if (avgPrice === 0) {
+        result = new Error(
+          `not enough liquidity on ${this.name} for ${desiredAmount} ${symbol}`,
+        )
+      } else {
+        result = {
+          amount: desiredAmount,
+          symbol,
+          avgPrice,
+        }
+      }
+
+      return { [this.name]: result }
+    }
+  }
+}
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const rp = require('request-promise')
-const { PARADEX_URL, IDEX_URL } = require('./constants.js')
+const IDEX = require('./exchanges/IDEX.js')
 
 // canonical order book interface
 const exampleBook = {
@@ -17,107 +16,6 @@ const exampleBook = {
   ],
 }
 
-class IDEX {
-  constructor() {
-    this.url = `${IDEX_URL}/returnOrderBook`
-    this.name = 'IDEX'
-  }
-
-  // fetch the raw order book from the exchange
-  getRawOrderBook(symbol) {
-    const body = {
-      market: `ETH_${symbol}`,
-      count: 50,
-    }
-    const config = {
-      uri: this.url,
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body,
-      json: true,
-    }
-
-    return rp(config)
-  }
-
-  // create an order book that conforms to the generalized order book interface
-  async createCanonicalOrderBook(symbol) {
-    let lotPrice = 0
-    let lotAmount = 0
-    function walkBook(level) {
-      const levelPrice = parseFloat(level.price)
-      const levelAmount = parseFloat(level.amount)
-
-      lotAmount += levelAmount
-      lotPrice += levelPrice * levelAmount
-
-      const avgUnitPrice = lotPrice / lotAmount
-      return {
-        levelPrice,
-        levelAmount,
-        lotPrice,
-        lotAmount,
-      }
-    }
-
-    return new Promise(async resolve => {
-      try {
-        const book = await this.getRawOrderBook(symbol)
-        const { asks, bids } = book
-
-        const formattedAsks = asks.map(walkBook)
-        lotPrice = 0
-        lotAmount = 0
-        const formattedBids = bids.map(walkBook)
-        resolve({ asks: formattedAsks, bids: formattedBids })
-      } catch (error) {
-        resolve(null)
-      }
-    })
-  }
-
-  // compute the average token price based on DEX liquidity and desired token amount
-  async computePrice(symbol, desiredAmount) {
-    const book = await this.createCanonicalOrderBook(symbol)
-
-    if (!book) {
-      return {
-        [this.name]: new Error(
-          `no price data found on ${this.name} for ${symbol}`,
-        ),
-      }
-    } else {
-      const { asks } = book
-      let avgPrice = 0
-
-      for (let i = 0; i < asks.length; i++) {
-        const ask = asks[i]
-        const prevAsk = asks[i - 1]
-
-        if (desiredAmount > ask.lotAmount) {
-          continue
-        }
-
-        if (i === 0) {
-          avgPrice = ask.levelPrice
-          break
-        }
-        const remainder = desiredAmount - prevAsk.lotAmount
-        const totalPrice = prevAsk.lotPrice + remainder * ask.levelPrice
-        avgPrice = totalPrice / desiredAmount
-        break
-      }
-
-      const response = {
-        amount: desiredAmount,
-        symbol,
-        avgPrice,
-      }
-      return { [this.name]: response }
-    }
-  }
-}
-
 // given a token symbol and amount, return offers from all dexes
 // sorted descending by best price
 function main(symbol, amount) {
@@ -133,7 +31,7 @@ function main(symbol, amount) {
   })
 }
 
-main('AURA', 10000)
+main('AURA', 1000000000000000)
 
 // const options = {
 //   uri: `${PARADEX_URL}/ticker?market=weth-dai`,
