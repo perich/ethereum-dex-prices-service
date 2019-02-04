ae4743b7a1c9263bf757aa8cc16364559957f75b
Neuron <neuron@rados.io>
2019-02-04 20:27:02 +0000

    Add Saturn Network

--- constants.js
+++ constants.js
@@ -5,6 +5,7 @@ const BANCOR_URL = 'https://api.bancor.network/0.1'
 const DDEX_URL = 'https://api.ddex.io/v3'
 const RADAR_RELAY_URL = 'https://api.radarrelay.com/v2'
 const ETHFINEX_URL = 'https://api.ethfinex.com/v1'
+const SATURN_URL = 'https://ticker.saturn.network/api/v2'
 
 const AIRSWAP_TOKEN_METADATA_URL = 'https://token-metadata.production.airswap.io'
 
@@ -49,5 +50,6 @@ module.exports = {
   RADAR_RELAY_URL,
   AIRSWAP_TOKEN_METADATA_URL,
   ETHFINEX_URL,
+  SATURN_URL,
   TOP_TOKENS_DECIMAL_MAP,
 }
